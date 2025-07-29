9669df61ad233a4df4631d84ef6dd0ac8a0ed3f8
SayLongGuan <64540846+SayLongGuan@users.noreply.github.com>
2025-07-29 22:37:13 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+import React from "https://esm.sh/react@18";
+import ReactDOM from "https://esm.sh/react-dom@18";
+import WarehouseBK from "./WarehouseBK.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  React.createElement(WarehouseBK)
+);
