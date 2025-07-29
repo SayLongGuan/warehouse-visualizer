33f56a5477aeb928899e8697ba0d19a22ba0f0ec
SayLongGuan <64540846+SayLongGuan@users.noreply.github.com>
2025-07-29 22:42:35 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-
-import React from "https://esm.sh/react@18";
-import ReactDOM from "https://esm.sh/react-dom@18";
-import WarehouseBK from "./WarehouseBK.js";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  React.createElement(WarehouseBK)
-);
